340a914ca567b528193c353e73597ea23011bfc3
jtcb <jbrule@cs.umd.edu>
2018-02-26 13:55:19 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to acausal
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/acausal/core_test.clj
@@ -0,0 +1,7 @@
+(ns acausal.core-test
+  (:require [clojure.test :refer :all]
+            [acausal.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
